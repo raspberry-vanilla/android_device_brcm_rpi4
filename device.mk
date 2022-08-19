203db632fa2c9202cdf5eed884aa6ca600fac137
Konsta <konsta09@gmail.com>
2022-08-19 12:48:10 +0300

    drm: switch clearkey to aidl

--- device.mk
+++ device.mk
@@ -126,9 +126,7 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := false
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # Ethernet
 PRODUCT_COPY_FILES += \
