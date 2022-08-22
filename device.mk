7c245cd0458948930f3e3330f9b2a6a8feada468
Konsta <konsta09@gmail.com>
2022-08-22 19:40:12 +0300

    switch bluetooth audio to aidl

--- device.mk
+++ device.mk
@@ -84,7 +84,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default
 
 PRODUCT_COPY_FILES += \
