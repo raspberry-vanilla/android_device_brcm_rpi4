2960d80f8d1a72f2c0729becdeffdd9c4dd274ea
nename0 <26363498+nename0@users.noreply.github.com>
2023-03-03 22:43:09 +0100

    hdmi audio: add new audio HAL module with alsa instead of tinyalsa

--- device.mk
+++ device.mk
@@ -25,6 +25,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
     audio.primary.rpi \
+    audio.primary.rpi_hdmi \
     audio.r_submix.default \
     audio.usb.default
 
