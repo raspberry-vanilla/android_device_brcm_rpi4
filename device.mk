18cc1bd154d68595e3708884b6262ae74d3905ac
Konsta <konsta09@gmail.com>
2023-07-27 10:30:07 +0300

    bluetooth: switch to btlinux

--- device.mk
+++ device.mk
@@ -71,9 +71,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service \
-    libbt-vendor
+    android.hardware.bluetooth@1.1-service.btlinux
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
