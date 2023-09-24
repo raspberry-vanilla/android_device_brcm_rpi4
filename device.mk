1ac293a58164d5c3bd86842c7613261f98e3dd25
Konsta <konsta09@gmail.com>
2023-09-24 16:29:10 +0300

    usb: switch to aidl

--- device.mk
+++ device.mk
@@ -245,7 +245,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb-service.example
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
