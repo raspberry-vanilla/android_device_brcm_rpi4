2df5bd40b403ce49381a8b229a8b354bdb7e11fd
Konsta <konsta09@gmail.com>
2023-10-07 13:49:10 +0300

    wifi: switch to aidl
    * Remove android.system.wifi.keystore from compatibility matrix
      as it's been removed in Android 14.

--- device.mk
+++ device.mk
@@ -264,7 +264,7 @@ $(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     hostapd_cli \
     libwpa_client \
