e288798b65f42b97b248dbc04780cfb4878e9f27
Konsta <konsta09@gmail.com>
2025-03-15 00:01:58 +0200

    light: convert to apex

--- device.mk
+++ device.mk
@@ -217,7 +217,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.rpi
+    com.android.hardware.light.rpi4
 
 # Media
 PRODUCT_COPY_FILES += \
--- light/main.cpp
+++ light/main.cpp
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2020 The Android Open Source Project
+ * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2025 KonstaKANG
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,5 +32,5 @@ int main() {
     CHECK(status == STATUS_OK);
 
     ABinderProcess_joinThreadPool();
-    return EXIT_FAILURE;  // should not reached
+    return EXIT_FAILURE;  // should not reach
 }
