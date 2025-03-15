7d91c1b33cd95177bd222b15dfcdd83c74bf9e70
Konsta <konsta09@gmail.com>
2025-03-15 15:38:22 +0200

    bluetooth: convert to apex
    * Use device as an identifier in package names so APEXs on rpi4/rpi5 trees
      can co-exist. Somehow apex definitions escape the soong namespace. It
      doesn't complain about duplicate package names but dependecies later in
      the build after analyzing Android.bp files and generating ninja file.
      FAILED: ninja: ... multiple rules generate com.android.hardware.bluetooth.rpi-deps-info [-w dupbuild=err]
      https://android.googlesource.com/platform/build/soong/+/refs/heads/main/README.md#namespaces
    * Not sure if this is an AOSP bug or intended behaviour for apex targets.
      Don't like using the rpi4/rpi5 tags but couldn't come up with better
      solution to limit the visibility that worked.
    * General cleanups. As APEX package contains all dependencies that are
      needed for the service, remove unused shared libraries.
      Run 'bpfmt -s -w Android.bp'.

--- device.mk
+++ device.mk
@@ -74,7 +74,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth-service.rpi
+    com.android.hardware.bluetooth.rpi4
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
