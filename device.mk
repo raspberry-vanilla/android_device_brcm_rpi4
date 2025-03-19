4ffea81903296f6a0d6715efd704dfa23429b225
Konsta <konsta09@gmail.com>
2025-03-19 12:59:47 +0200

    suspend_blocker: convert to apex
    * Start the service later at 'class hal' to avoid making this into bootstrap
      APEX. 'class early_hal' is started on trigger 'on late-fs' which is too
      early for vendor APEX.

--- device.mk
+++ device.mk
@@ -249,7 +249,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Suspend
 PRODUCT_PACKAGES += \
-    suspend_blocker_rpi
+    com.android.hardware.suspend_blocker.rpi4
 
 # Thermal
 PRODUCT_PACKAGES += \
