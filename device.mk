08303135d2adfbbac83a01253578ce9589fe03db
Konsta <konsta09@gmail.com>
2025-03-21 23:22:23 +0200

    hdmi: connection: convert to apex

--- device.mk
+++ device.mk
@@ -139,7 +139,7 @@ PRODUCT_COPY_FILES += \
 # CEC
 PRODUCT_PACKAGES += \
     com.android.hardware.tv.hdmi.cec.rpi4 \
-    android.hardware.tv.hdmi.connection-service.rpi
+    com.android.hardware.tv.hdmi.connection.rpi4
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.hdmi.cec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.hdmi.cec.xml
