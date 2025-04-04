884800068933a78f93ce2c29c136558440e79de0
Konsta <konsta09@gmail.com>
2025-04-05 00:03:40 +0300

    add empty product manifest
    * Needed with enforce-product-packages-exist.
    * https://android.googlesource.com/device/google/cuttlefish/+/c38cec789c28f6f98547c2b63ba2a62b6d8cbf41

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,7 @@ BOARD_KERNEL_CMDLINE := console=ttyS0,115200 no_console_suspend root=/dev/ram0 r
 # Manifest
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+PRODUCT_MANIFEST_FILES := $(DEVICE_PATH)/product_manifest.xml
 
 # Partition sizes
 BOARD_FLASH_BLOCK_SIZE := 4096
