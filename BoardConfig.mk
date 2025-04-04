theproduct-space/android_device_brcm_rpi5

set hardware platform name
* Some hardware information applications use this so set actual hardware
  platform name.
* Historic reason why this was set to rpi is that this allowed to use same
  HALs on rpi3/rpi4 when they were commonized. 'ro.board.platform' this sets
  is used in the legacy module load order.
  https://android.googlesource.com/platform/hardware/libhardware/+/refs/tags/android-15.0.0_r20/modules/README.android
  This has no use with modern HIDL/AIDL/APEX HALs. For legacy HALs name can
  be implicitly set using system properties (e.g. 'ro.hardware.audio.primary'
  that's still relevant).

BoardConfig.mk
@@ -61,7 +61,7 @@ TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
 TARGET_USERIMAGES_USE_EXT4 := true
 
 # Platform
-TARGET_BOARD_PLATFORM := rpi
+TARGET_BOARD_PLATFORM := bcm2712
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop

raspberry-vanilla/android_device_brcm_rpi4

BoardConfig.mk
@@ -61,7 +61,7 @@ TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
 TARGET_USERIMAGES_USE_EXT4 := true
 
 # Platform
-TARGET_BOARD_PLATFORM := rpi
+TARGET_BOARD_PLATFORM := bcm2711
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop