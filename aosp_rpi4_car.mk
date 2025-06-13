6da2011d045026c6c471f114bde530639b554b5f
Konsta <konsta09@gmail.com>
2025-06-13 19:29:20 +0300

    car: update vehicle aidl to v4

--- aosp_rpi4_car.mk
+++ aosp_rpi4_car.mk
@@ -104,7 +104,7 @@ PRODUCT_COPY_FILES += \
 
 # Vehicle
 PRODUCT_PACKAGES += \
-    android.hardware.automotive.vehicle@V3-default-service
+    android.hardware.automotive.vehicle@V4-default-service
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := rpi4
