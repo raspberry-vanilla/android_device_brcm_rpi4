dbb03fcd99aee9ceab47d477cf2ecf28163fd6cb
Konsta <konsta09@gmail.com>
2025-06-13 23:00:37 +0300

    bump shipping api level

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@ $(call inherit-product, vendor/brcm/rpi4/rpi4-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # API level
-PRODUCT_SHIPPING_API_LEVEL := 35
+PRODUCT_SHIPPING_API_LEVEL := 36
 
 # Audio
 PRODUCT_PACKAGES += \
