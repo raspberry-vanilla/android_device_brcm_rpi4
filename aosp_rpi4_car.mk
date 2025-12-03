0bfe6b42c1389a598c555bdeada48b946ce7f6e4
Konsta <konsta09@gmail.com>
2025-12-03 16:25:15 +0200

    car: update exceptions for non-existing targets for AOSP16 QPR2

--- aosp_rpi4_car.mk
+++ aosp_rpi4_car.mk
@@ -15,7 +15,7 @@ PRODUCT_CHARACTERISTICS := automotive,nosdcard
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, packages/services/Car/car_product/build/car.mk)
-$(call enforce-product-packages-exist,Bluetooth Keyguard Launcher2 OverviewApp RotaryIME RotaryPlayground com.android.ranging libnfc_ndef libvariablespeed pppd)
+$(call enforce-product-packages-exist,Bluetooth Keyguard Launcher2 OverviewApp RotaryIME RotaryPlayground com.android.ranging display_compat_config libnfc_ndef libvariablespeed pppd)
 
 # android.car
 PRODUCT_PACKAGES += \
