99ff5b226fdc9aef7cbec72736a58b959ba58619
Konsta <konsta09@gmail.com>
2026-03-14 19:02:14 +0200

    clean up cmdline
    * See [1] for documentation.
    
    [1]: https://docs.kernel.org/admin-guide/kernel-parameters.html

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@ TARGET_SCREEN_DENSITY := 240
 # Kernel
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
-BOARD_KERNEL_CMDLINE := console=ttyS0,115200 no_console_suspend root=/dev/ram0 rootwait androidboot.hardware=rpi4
+BOARD_KERNEL_CMDLINE := console=ttyS0,115200 androidboot.hardware=rpi4
 
 # Manifest
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
